dc02a587190622cf67113b02dded837f68224277
Michael Lawinger <sychaellawinger@gmail.com>
2017-09-21 18:11:41 -0500

    2nd Update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 function loadScript(url) {
   var head = document.getElementsByTagName('head')[0];
   var script = document.createElement('script');
-  script.type = 'text/javascript';
   script.src = url;
   head.appendChild(script);
 }
